87840ed2a012a91177c86f36bb07affed6b8bc91
lizhiwei <2894246564@qq.com>
2023-09-18 15:16:38 +0800

    add Poller.cpp

--- Poller.cpp
+++ Poller.cpp
@@ -1,7 +1,13 @@
 #include "Poller.h"
+#include "Channel.h"
 
 Poller::Poller(EventLoop *loop) 
     : ownerloop_(loop)
 {
+}
 
+bool hasChannel(Channel *channel) const
+{
+    auto it = channels_.find(channel->fd());
+    return it != channels_.end() && it->second == channel;
 }
