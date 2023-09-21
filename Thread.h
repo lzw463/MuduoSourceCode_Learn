ea515572513dac5cbd0bde6920adca3642b6b639
lizhiwei <2894246564@qq.com>
2023-09-21 16:34:06 +0800

    add class Thread

---
+++ Thread.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "noncopyable.h"
+
+#include <functional>
+#include <thread>
+#include <memory>
+#include <unistd.h>
+#include <string>
+
+class Thread : noncopyable
+{
+public:
+    using ThreadFunc = std::function<void()>;
+
+    explicit Thread(ThreadFunc, const std::string &name = std::string());
+    ~Thread();
+
+    void start();
+    void join();
+
+    bool started() const { return started_;}
+    pid_t tid() const { return tid_;}
+    const std::string& name() const { return name_;}
+
+    static int numCreated() {return numCreated_;}
+private:
+    void setDefaultName();
+
+    bool started_;
+    bool joined_;
+    //std::thread thread_; //定义就启动，需要用智能指针掌控资源
+    std::shared_ptr<std::thread> thread_;
+    pid_t tid_;
+    ThreadFunc func_;
+    std::string name_;
+    static std::atomic_int numCreated_;
+     
+    
+};
