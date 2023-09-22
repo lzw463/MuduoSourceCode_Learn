8d6a1445bf3f1530d337eaeef6aa7720ab6ef686
lizhiwei <2894246564@qq.com>
2023-09-22 16:02:15 +0800

    add EventLoopThreadPool class

---
+++ EventLoopThreadPool.cpp
@@ -0,0 +1,67 @@
+#include "EventLoopThreadPool.h"
+#include "EventLoopThread.h"
+
+#include <memory>
+ 
+ 
+EventLoopThreadPool::EventLoopThreadPool(EventLoop *baseLoop, const std::string &nameArg)
+    : baseLoop_(baseLoop)
+    , name_(nameArg)
+    , started_(false)
+    , numThreads_(0)
+    , next_(0)
+{}
+
+EventLoopThreadPool::~EventLoopThreadPool()
+{}  
+
+void EventLoopThreadPool::start(const ThreadInitCallback &cb)
+{
+    started_ = true;
+
+    for (int i = 0; i < numThreads_; ++i)
+    {
+        char buf[name_.size() + 32];
+        snprintf(buf, sizeof buf, "%s%d", name_.c_str(), i);
+        EventLoopThread *t = new EventLoopThread(cb, buf);
+        threads_.push_back(std::unique_ptr<EventLoopThread>(t));
+        loops_.push_back(t->startLoop());   //底层创建线程，绑定一个新的Eventloop
+
+    }
+
+    //整个服务端只有一个线程，运行着mainloop
+    if (numThreads_ == 0 && cb)
+    {
+        cb(baseLoop_);
+    }
+}
+
+
+//轮询选择subloop
+EventLoop* EventLoopThreadPool::getNextLoop()
+{
+    EventLoop *loop = baseLoop_;
+    if (!loops_.empty())
+    {
+        loop = loops_[next_];
+        ++next_;
+        if (next_ >= loops_.size())
+        {
+            next_ = 0;
+        }
+    }
+
+    return loop;
+}
+
+std::vector<EventLoop*> EventLoopThreadPool::getAllLoops()
+{
+    if (loops_.empty())
+    {
+        return std::vector<EventLoop*> (1, baseLoop_);
+    }
+    else
+    {
+        loops_;
+    }
+}
