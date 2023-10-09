8659ce1bc546942500cd88a83f8b726116cbfab1
lizhiwei <2894246564@qq.com>
2023-10-09 14:17:32 +0800

    add TcpServer

--- TcpServer.cpp
+++ TcpServer.cpp
@@ -1,1 +1,52 @@
 #include "TcpServer.h"
+#include "Logger.h"
+#include "Acceptor.h"
+#include "EventLoopThreadPool.h"
+
+#include <functional>
+    
+EventLoop* CheckLoopNotNull(EventLoop *loop)
+{
+    if (loop == nullptr)
+    {
+        LOG_FATAL("%s:%s:%d mainloop is null \n", __FILE__, __FUNCTION__, __LINE__);
+    }
+    return loop;
+}
+
+TcpServer::TcpServer(EventLoop *loop,
+                const InetAddress &listenAddr,
+                const std::string &nameArg,
+                Option option = kNoReusePort)
+                : loop_(CheckLoopNotNull(loop))
+                , ipPort_(listenAddr.toIpPort())
+                , name_(nameArg)
+                , acceptor_(new Acceptor(loop, listenAddr, option == kReusePort))
+                , threadPool_(new EventLoopThreadPool(loop, name_))
+                , connectionCallback_()
+                , messageCallback_()
+                , nextConnId_(1)
+{
+    //当有新用户连接时，会执行Tcpserver::newconnection回调
+    acceptor_->setNewConnectionCallback(std::bind(&TcpServer::newConnection, this, std::placeholders::_1, std::placeholders::_2));
+
+} 
+
+void TcpServer::newConnection(int sockfd, const InetAddress &peerAddr)//轮询
+{
+
+}
+
+void TcpServer::setThreadNum(int numThreads)
+{
+    threadPool_->setThreadNum(numThreads);
+}
+
+void TcpServer::start()
+{
+    if (started_++ == 0) //防止一个对象被start多次
+    {
+        threadPool_->start(threadInitCallback_);
+        loop_->runInLoop(std::bind(&Acceptor::listen, acceptor_.get()));
+    }
+} 
